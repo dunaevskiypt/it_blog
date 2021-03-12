71c84245e4b46f8dd401ac486568aba68debf4f6
Peter Dunavskiy <dunaevskiyp@gmail.com>
2021-03-12 14:59:27 +0200

    friendFinalitem

--- src/components/Navbar/Friends/FriendProfile/FriendProfile.jsx
+++ src/components/Navbar/Friends/FriendProfile/FriendProfile.jsx
@@ -7,9 +7,10 @@ import style from "./FriendProfile.module.css";
 function FriendProfile (props) {
     return(
         <div className={style.friendProfile_wrapper}>
-            <div className={style.item}></div>
+            <div className={style.item}>
            <NavLink id={props.id} to="/profile"><div className={style.logo}><img src={props.foto} alt="foto"/></div></NavLink>
            <span>{props.name}</span>
+           </div>
         </div>
     )
 }
