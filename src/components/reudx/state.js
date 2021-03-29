94f5eb2d38156596b31f4aca5d6b56a4000ed744
dunaevskiypt <dunaevskiyp@gmail.com>
2021-03-29 23:44:27 +0300

    REDUCER

--- src/components/reudx/state.js
+++ src/components/reudx/state.js
@@ -1,3 +1,7 @@
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+import sideBarReducer from "./sideBar-reducer";
+
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE-NEW-MESSAGE-BODY";
@@ -91,6 +95,7 @@ let store = {
         },
       ],
     },
+    sideBar: {},
   },
   _callSubsriber() {
     console.log("state was changed");
@@ -102,27 +107,11 @@ let store = {
     return this._state;
   },
   dispatch(action) {
-    if (action.type === ADD_POST) {
-      let newPost = {
-        id: 5,
-        message: this._state.profilePage.newPostEText,
-        likeCount: 0,
-      };
-      this._state.profilePage.posts.push(newPost);
-      this._state.profilePage.newPostText = "";
-      this._callSubsriber(this._state);
-    } else if (action.type === UPDATE_NEW_POST_TEXT) {
-      this._state.profilePage.newPostEText = action.newText;
-      this._callSubsriber(this._state);
-    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-      this._state.dialogsPage.newMessageBody = action.body;
-      this._callSubsriber(this._state);
-    } else if (action.type === SEND_MESSAGE) {
-      let body = this._state.dialogsPage.newMessageBody;
-      this._state.dialogsPage.newMessageBody = "";
-      this._state.dialogsPage.messages.push({ id: 6, message: body });
-      this._callSubsriber(this._state);
-    }
+    this._state.profilePage = profileReducer(this._state.profilePage, action);
+    this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+    this._state.sideBar = sideBarReducer(this._state.sideBar, action);
+
+    this._callSubsriber(this._state);
   },
 };
 export const PostActionCreator = () => ({ type: ADD_POST });
